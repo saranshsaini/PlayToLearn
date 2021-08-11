c0169a7d5f45dd066ff149ad7cdb622655816268
Saransh Saini <saranshs@berkeley.edu>
2021-08-11 17:31:52 -0400

    fixed typo

--- src/Instructions.js
+++ src/Instructions.js
@@ -4,21 +4,23 @@ export default function Instructions(props) {
   return (
     <div className="instructions-box">
       <h2>Set up</h2>
+      <p>
+        Want to learn how to read squiggles on paper and somehow know what key
+        to press? Me too!
+      </p>
       <p>You'll need a MIDI capable piano to make the most of this site.</p>
       <p>
         Currently, only Chrome and Edge have MIDI Input capability supported in
         the browser.
       </p>
-      <p>
-        Enter the number of notes you want to play and hit start!
-      </p>
+      <p>Enter the number of notes you want to play and hit start!</p>
       <p>
         If the MIDI input isn't being detected for whatever reason, simply
-        unplug and plug it back into you're computer.
+        unplug and plug it back into your computer.
       </p>
       <p>
-        The image below shows various connection types. The most straightforward
-        is the USB connection, though others can work as well.
+        The image below shows various connection types. The USB connection is
+        the most straightforward, though other types work as well.
       </p>
       <img
         src="https://www.doctormix.com/blog/wp-content/uploads/2016/05/What-Audio-Cable-Is-Best.jpg"
