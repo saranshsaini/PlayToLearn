26509351513d2d5e685a81cce9a01f458c524b98
Saransh Saini <saranshs@berkeley.edu>
2021-08-24 13:33:52 -0700

    edit header

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header-holder">
      
-      <h1>Piano Typer</h1>
+      <h1>Piano Typer  &lt;Beta&gt; </h1>
      
     </div>
   );
